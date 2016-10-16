a385e8eb55e35a75155e24ae12ec2b93238b6e39
ninale91 <ninale@Ninas-MacBook-Air.local>
2016-10-16 18:13:14 +1100

    button changing classes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 $(document).ready(start);
 
 function start(){
-    $( '.dropdown' ).hover(openMenu);
+    $('.dropdown' ).hover(openMenu);
+    $('.button-done').click(completedFood);
 }
 
 function openMenu() {
   $('.sub-menu').slideToggle();
+}
+
+function completedFood() {
+  $('.post-wrapper').toggleClass('eaten');
+  $('.food-image').toggleClass('eaten');
+  $('.resturant-location').toggleClass('eaten');
 }
